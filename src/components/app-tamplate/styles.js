dd188adec7c05d868899443ae4eedd48e331889a
Your Name <youremail@example.com>
2026-04-27 00:51:35 +0300

    the  tamplate of site

--- src/components/app-tamplate/styles.js
+++ src/components/app-tamplate/styles.js
@@ -4,16 +4,13 @@ export const Styles = styled.div`
   .main-data {
     min-height: calc(100dvh - 100px);
     /* margin-left: 500px; */
-
   }
-  .layout-wrapper{
-    display: 'flex';
+  .layout-wrapper {
+    display: "flex";
     min-height: 100vh;
-  position: relative;
+    position: relative;
   }
-  .main-content{
-    flex: 1 ;
+  .main-content {
+    flex: 1;
   }
-
-  main-data
 `;
